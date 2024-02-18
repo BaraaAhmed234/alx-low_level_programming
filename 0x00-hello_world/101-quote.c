395181791830f94c0942cf97063fef687499cfbb
BaraaAhmed234 <brooahmed234@gmail.com>
2024-02-18 13:31:23 -0800

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,18 +1,11 @@
-#include <stdio.h>
+#include <unistd.h>
 #include <stdib.h>
-#include <string.h>
-#include <fctl.h>
-#include <errno.h>
-
-#define STDERR_FILENO 2
 
 int main()
 {
-	const char *message = "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n";
-	if (write(STDERR_FILENO, message, len) != len)
+	if (write(STDERR_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 56) != 56)
 {
-	/* handle error*/
-	return 1;
-}
         return 1;
+}
+        return 1:
 }
