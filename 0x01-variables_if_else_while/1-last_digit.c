f8c53bb3df0e2436515eac2295ee29fc575ba3a8
BaraaAhmed234 <brooahmed234@gmail.com>
2024-02-20 08:25:16 -0800

    last-digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,12 +11,12 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-	int n;
-	int lastdigit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
+	 int n;
+        int lastdigit;
 
 	lastdigit = n % 10;
 
