73da37693ea25c451668d8902a2420edcaa673f3
BaraaAhmed234 <brooahmed234@gmail.com>
2024-02-29 09:48:41 -0800

    2-print_alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,9 +2,13 @@
 /* my code goes here */
 
 int main(void){
+	/* Declare a variable and initialize it with the first member of the range [a - z]*/
+
 	char alphabet = 'a';
-	while (alphabet <= 'z'){
+	while (alphabet <= 'z')
+	{
 		putchar(alphabet);
+
 		alphabet++;
 	}
 	putchar('\n');
