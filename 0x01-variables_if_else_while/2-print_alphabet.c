7ac9d67fcd0d376b0a738bf90718e2baa2be4b05
BaraaAhmed234 <brooahmed234@gmail.com>
2024-02-29 09:29:56 -0800

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/* my code goes here */
+
+int main(void){
+	char alphabet = 'a';
+	while (alphabett <= 'z'){
+		putchar(alphabet);
+		alphabet++;
+	}
+	putchar("\n");
+	return (0);
+}
