6ca92c236e1ec8a7eff360e52a147df9323101cb
BaraaAhmed234 <brooahmed234@gmail.com>
2024-03-02 11:44:31 -0800

    3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main Entry
+ * always return 0 (success)
+ *
+ */
+int main()
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		putchar(i); // lower case alphabet
+	}
+
+	putchar('\n'); // new line 
+
+	for (i = 65; i < 91; i++)
+	{
+		putchar(i); // uppercase alphabet
+	}
+
+	putchar('\n'); // new line
+
+	return (0);
+}
