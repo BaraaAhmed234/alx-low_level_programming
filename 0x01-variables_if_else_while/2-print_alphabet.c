a61d705ee3da9cd011789d4568cb30d41c3afd80
BaraaAhmed234 <brooahmed234@gmail.com>
2024-03-02 12:28:19 -0800

    2-print_alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
 /**
- * Main - this is the main function 
+ * main - this is the main function 
  *
  * Return : always 0(success)
  */
 /* betty style doc for function main goes here */
-
 int main(void)
 {
-	/* Declare a variable and initialize it with the first member of the range [a - z] */
-
-	char alphabet = 'a';
+        char alphabet = 'a';
 	while (alphabet <= 'z')
 	{
 		putchar(alphabet);
