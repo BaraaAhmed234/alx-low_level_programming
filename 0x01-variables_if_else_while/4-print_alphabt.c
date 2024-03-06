cb0bed9250ba62df7ed1829141a43d527e4a434b
BaraaAhmed234 <brooahmed234@gmail.com>
2024-03-06 14:57:50 -0800

    print alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - this the main function
+ *
+ * Return : always 0(success)
+ */
+/* betty style doc for function main goes here */
+int main(void)
+{
+	char letter;
+
+	while (letter = 'a' letter <= 'z' letter++)
+	{
+	if (letter != 'q' && letter != 'e')
+	{
+		putchar(letter);
+	}
+	}
+	putchar('\n');
+
+	return 0;
+}
+
