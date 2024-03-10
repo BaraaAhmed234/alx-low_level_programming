06709f423eaac18c5ab9c2e865915d82ea70ab4b
BaraaAhmed234 <brooahmed234@gmail.com>
2024-03-10 09:11:43 -0700

    7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - this is the main function
+ * Return : always 0 (success)
+ */
+/* betty style doc for main function goes here */
+int main(void)
+{
+	int y;
+
+	for (y = 'z'; y >= 'a'; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+}
